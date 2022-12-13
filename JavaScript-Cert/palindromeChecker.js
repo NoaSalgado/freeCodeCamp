7ec06b2fa98f1de50455a0ecdf95ec86d9e53cfc
Noa <noasalgadodev@gmail.com>
2022-12-13 11:13:06 +0100

    Solve palindrome checker challenge

---
+++ JavaScript-Cert/palindromeChecker.js
@@ -0,0 +1,16 @@
+/*
+Devuelve true si la cadena proporcionada es un palíndromo. De lo contrario, devuelve false.
+
+Un palíndromo es una palabra o frase que se escribe de la misma manera hacia adelante y hacia atrás, ignorando la puntuación, mayúsculas, minúsculas y espaciado.
+
+Nota: Tendrás que eliminar todos los caracteres no alfanuméricos (puntuación, espacios y símbolos) y convertir todo en mayúsculas o minúsculas para comprobar si hay palíndromos.
+
+Pasaremos cadenas con formatos variables, como racecar, RaceCar y race CAR entre otros.
+
+También pasaremos cadenas con símbolos especiales, como 2A3*3a2, 2A3 3a2 y 2_A3*3#A2.
+*/
+
+function isPalindrom(str) {
+  const cleanedString = str.replaceAll(/\W|_/g, "").toLowerCase();
+  return cleanedString === cleanedString.split("").reverse().join("");
+}
